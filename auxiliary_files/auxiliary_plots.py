2abd9aea382f840cea6587cb6a2fe6fe98a0a55b
s6emschw <64075224+s6emschw@users.noreply.github.com>
2022-01-09 13:49:58 +0100

    Adjusted plot fontsizes.

--- auxiliary_files/auxiliary_plots.py
+++ auxiliary_files/auxiliary_plots.py
@@ -24,16 +24,18 @@ def plot_betas(dfs, alphas, reg_type):
         ax.set_xscale("log")
         ax.set_xlim(ax.get_xlim()[::-1])  # reverse axis
     
-        plt.xlabel(f"$\lambda$", fontsize = 25)
-        plt.ylabel("weights", fontsize = 25)
+        plt.xlabel(f"$\lambda$", fontsize = 35)
+        plt.ylabel("weights", fontsize = 35)
         plt.axhline(y=0, color='black', linestyle='--')
     
-        ax.tick_params(axis='both', which='major', labelsize = 20)
+        ax.tick_params(axis='both', which='major', labelsize = 32)
 
-        plt.title(f"{reg_type} coefficients as a function of $\lambda$, p = {i[1][0].shape[1]} ", fontsize = 28)
+        plt.title(f"{reg_type} coefficients as a function of $\lambda$, p = {i[1][0].shape[1]} ", fontsize = 40)
         plt.axis("tight")
+    
+    plt.savefig(f"{reg_type}_plot_betas.png", dpi=500, bbox_inches='tight')
         
-def plot_average_betas(dfs, alphas, L_w, reg_type):
+def plot_average_betas(dfs, alphas, reg_type):
 
     plt.figure(figsize = (40, 25))
     count = 1
@@ -48,21 +50,15 @@ def plot_average_betas(dfs, alphas, L_w, reg_type):
         ax.set_xscale("log")
         ax.set_xlim(ax.get_xlim()[::-1])  # reverse axis
     
-        plt.xlabel("alpha", fontsize = 25)
-        plt.ylabel("weights", fontsize = 25)
+        plt.xlabel("alpha", fontsize = 35)
+        plt.ylabel("weights", fontsize = 35)
     
-        ax.tick_params(axis='both', which='major', labelsize = 20)
+        ax.tick_params(axis='both', which='major', labelsize = 32)
      
         plt.axhline(y=0, color='black', linestyle='--')
         plt.axis("tight")
         
-        if reg_type == "Average elastic net":
-            
-            plt.title(f"{reg_type} coefficients as a function of $\lambda$, p = {i.shape[1]}, L_ratio = {L_w} ", fontsize = 28)
-        
-        else:
-           
-            plt.title(f"{reg_type} coefficients as a function of $\lambda$, p = {i.shape[1]} ", fontsize = 28)
+        plt.title(f"{reg_type} coefficients as a function of $\lambda$, p = {i.shape[1]} ", fontsize = 40)
         
         count += 1
         
@@ -87,7 +83,7 @@ def plot_betas_single_case(df, alphas, L_w, reg_type):
     #plt.axhline(y=0, color='black', linestyle='--')
     if reg_type == "Average elastic net":
             
-            plt.title(f"{reg_type} coefficients as a function of $\lambda$, p = {df.shape[1]}, L_ratio = {L_w} ", fontsize = 28)
+            plt.title(f"{reg_type} coefficients as a function of $\lambda$, p = {df.shape[1]}, L_ratio = {L_w}[count-1] ", fontsize = 28)
         
     else:
            
@@ -139,25 +135,35 @@ def plot_ols_beta_distribution(df):
     fig_1.legend([],[], frameon=False)
     fig_1.axvline(x=5, color='black', linestyle='--')
     fig_1.spines["bottom"].set_linestyle("dotted")
-    fig_1.title.set_text(f"Distribution of OLS coefficients for p = {df[0].shape[1]}")
-
+    fig_1.tick_params(labelsize=25)
+    fig_1.set_ylabel("Density", fontsize=28)
+    fig_1.set_title(f"Distribution of OLS coefficients for p = {df[0].shape[1]}", fontsize=32)
+    
     fig_2=sns.kdeplot(data=df[1], ax=axes[0,1])
     fig_2.legend([],[], frameon=False)
     fig_2.axvline(x=5, color='black', linestyle='--')
     fig_2.spines["bottom"].set_linestyle("dotted")
-    fig_2.title.set_text(f"Distribution  of OLS coefficients for p = {df[1].shape[1]}")
+    fig_2.tick_params(labelsize=25)
+    fig_2.set_ylabel("Density", fontsize=28)
+    fig_2.set_title(f"Distribution  of OLS coefficients for p = {df[1].shape[1]}", fontsize=32)
 
     fig_3=sns.kdeplot(data=df[2], ax=axes[1,0])
     fig_3.legend([],[], frameon=False)
     fig_3.axvline(x=5, color='black', linestyle='--')
     fig_3.spines["bottom"].set_linestyle("dotted")
-    fig_3.title.set_text(f"Distribution of OLS coefficients for p = {df[2].shape[1]}")
+    fig_3.tick_params(labelsize=25)
+    fig_3.set_ylabel("Density", fontsize=28)
+    fig_3.set_title(f"Distribution of OLS coefficients for p = {df[2].shape[1]}", fontsize=32)
 
     fig_4=sns.kdeplot(data=df[3], ax=axes[1,1])
     fig_4.legend([],[], frameon=False)
     fig_4.axvline(x=5, color='black', linestyle='--')
     fig_4.spines["bottom"].set_linestyle("dotted")
-    fig_4.title.set_text(f"Distribution coefficients for p = {df[3].shape[1]}")
+    fig_4.tick_params(labelsize=25)
+    fig_4.set_ylabel("Density", fontsize=28)
+    fig_4.set_title(f"Distribution coefficients for p = {df[3].shape[1]}", fontsize=32)
+    
+    f.savefig('ols_distr.png', dpi=500, bbox_inches='tight')
     
     
 def plot_elnet_beta_distribution(df_low, df_med, df_high): 
