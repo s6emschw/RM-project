eb9d669fd6e87d1edfb2ceaba88e477bae3a494b
s6emschw <64075224+s6emschw@users.noreply.github.com>
2022-02-10 17:47:11 +0100

    Edited plot and analysis functions.

--- auxiliary_files/auxiliary_analysis.py
+++ auxiliary_files/auxiliary_analysis.py
@@ -12,52 +12,6 @@ from sklearn import linear_model
 
 
 
-def get_sim_data_old(n, p, q, min_cor, max_cor, true_betas):
-    
-    #p is the number of correlated regressors
-    #q is the number of uncorrelated regressors
-    #p+q= tot number of regressros
-    
-    sd_vec = np.ones(p) 
-    mean = np.zeros(p)
-    cor_matrix = np.zeros((p,p))
-
-    correlation = np.random.uniform(min_cor, max_cor, int(p * (p - 1) / 2))
-    cor_matrix[np.triu_indices(p, 1)] = correlation
-    cor_matrix[np.tril_indices(p, -1)] = cor_matrix.T[np.tril_indices(p, -1)]
-    np.fill_diagonal(cor_matrix, 1)
-
-
-    D = np.diag(sd_vec)
-    sigma = D.dot(cor_matrix).dot(D)
-
-    X_corr = np.random.multivariate_normal(mean, sigma, n)
-    if q>0:
-        X_uncorr = np.random.multivariate_normal(np.zeros(q), np.identity(q), n)
-        X = np.concatenate([X_corr, X_uncorr], axis=1) #X = pd.concat([X_corr, X_uncorr], axis=1)
-    else:
-        X = X_corr
-        
-    eps = np.random.normal(0, 1, n)
-
-    y = X.dot(true_betas) + eps 
-    
-    y = pd.Series(y, name = "y")
-    
-    column_names = []
-    
-    for value in range(1, p+q + 1): 
-        
-        column = f"X_{value}"
-        column_names.append(column)
-        
-    
-    X = pd.DataFrame(X, columns = column_names)
-    
-    df = pd.concat([y, X], axis = 1)
-    
-    return y, X, df
-
 def get_sim_data(n, p, cor_factor, true_betas):
     
     sd_vec = np.ones(p) 
@@ -103,12 +57,13 @@ def get_sim_data(n, p, cor_factor, true_betas):
     
     return y, X, df
 
-def iterate_ridge(n, p, q, min_cor, max_cor, iterations_sim, true_betas, alphas):
+
+def iterate_ridge(n, p, cor_factor, iterations_sim, true_betas, alphas):
     
     beta_var_names = []
     ridge_beta_names = []
     
-    for value in range(1, p + q + 1): 
+    for value in range(1, p + 1): 
     
         column_betas_var = f"beta_var_{value}"
         column_betas = f"beta_{value}"
@@ -122,7 +77,7 @@ def iterate_ridge(n, p, q, min_cor, max_cor, iterations_sim, true_betas, alphas)
         
         true_betas_sim = true_betas
     
-        y_noise, X, df = get_sim_data(n, p, q, min_cor, max_cor, true_betas_sim) 
+        y_noise, X, df = get_sim_data(n, p, cor_factor, true_betas_sim) 
         
         i, k = X.shape
         I = np.identity(k)
@@ -149,7 +104,7 @@ def iterate_ridge(n, p, q, min_cor, max_cor, iterations_sim, true_betas, alphas)
     return df_list_betas_ridge, df_list_var_ridge
 
 
-def iterate_lasso_sklearn(n, p, q, min_cor, max_cor, iterations_sim, true_betas, alphas):
+def iterate_lasso_sklearn(n, p, cor_factor, iterations_sim, true_betas, alphas):
     
     lasso_beta_names = []
     
@@ -163,7 +118,7 @@ def iterate_lasso_sklearn(n, p, q, min_cor, max_cor, iterations_sim, true_betas,
         
         true_betas_sim = true_betas
     
-        y_noise, X, df = get_sim_data(n, p, q, min_cor, max_cor, true_betas_sim) 
+        y_noise, X, df = get_sim_data(n, p, cor_factor, true_betas_sim) 
         matr_beta = []
     
         for a in alphas: 
@@ -179,39 +134,6 @@ def iterate_lasso_sklearn(n, p, q, min_cor, max_cor, iterations_sim, true_betas,
     return df_list_betas_lasso
 
 
-# delete iterate_elnet_old
-def iterate_elnet_old(n, p, q, min_cor, max_cor, iterations_sim, true_betas, alphas, L_w):
-    
-    elnet_beta_names = []
-    
-    for value in range(1, p+q + 1): 
-    
-        column_betas = f"beta_{value}"
-        elnet_beta_names.append(column_betas)
-
-    df_list_betas_elnet = []
-
-    for i in range(iterations_sim):
-        
-        true_betas_sim = true_betas
-    
-        y, X, df = get_sim_data_old(n, p, q, min_cor, max_cor, true_betas_sim) 
-        matr_beta = []
-        
-        #for L_w in L_weight:
-    
-        for a in alphas: 
-        
-            elnet_model = ElasticNet(alpha=a, l1_ratio=L_w).fit(X,y)
-            elnet_beta = np.array(elnet_model.coef_)
-            matr_beta.append(elnet_beta)
-            df_elnet_betas = pd.DataFrame(matr_beta, columns = elnet_beta_names)
-        
-         
-        df_list_betas_elnet.append(df_elnet_betas)
-        
-    return df_list_betas_elnet
-
 def iterate_elnet(n, p, cor_factor, iterations_sim, true_betas, alphas, L_w):
     
     elnet_beta_names = []
@@ -225,7 +147,7 @@ def iterate_elnet(n, p, cor_factor, iterations_sim, true_betas, alphas, L_w):
 
     for i in range(iterations_sim):
         
-        true_betas_sim = true_betas
+       # true_betas_sim = true_betas
     
         y, X, df = get_sim_data(n, p, cor_factor, true_betas) 
         matr_beta = []
@@ -244,10 +166,8 @@ def iterate_elnet(n, p, cor_factor, iterations_sim, true_betas, alphas, L_w):
         
     return df_list_betas_elnet
 
-# only need one general get_var_distribution function. 
 
-def get_ridge_var_distribution(df, iterations, alpha_low, alpha_med, alpha_high):
-
+def get_var_distribution(df, iterations, alpha_low, alpha_med, alpha_high):
 
     betas_low_alpha = []
     betas_med_alpha = []
@@ -270,53 +190,6 @@ def get_ridge_var_distribution(df, iterations, alpha_low, alpha_med, alpha_high)
     
     return df_low_a, df_med_a, df_high_a
 
-def get_lasso_var_distribution(df, iterations, alpha_low, alpha_med, alpha_high):
-
-
-    betas_low_alpha = []
-    betas_med_alpha = []
-    betas_high_alpha = []
-
-    for i in list(range(iterations)): 
-
-        betas_low_a = np.array(df[i].iloc[alpha_low, :])
-        betas_med_a = np.array(df[i].iloc[alpha_med, :])
-        betas_high_a = np.array(df[i].iloc[alpha_high, :])
-    
-        betas_low_alpha.append(betas_low_a)
-        betas_med_alpha.append(betas_med_a)
-        betas_high_alpha.append(betas_high_a)
-    
-        df_low_a = pd.DataFrame(betas_low_alpha)
-        df_med_a = pd.DataFrame(betas_med_alpha)
-        df_high_a = pd.DataFrame(betas_high_alpha)
-        
-    
-    return df_low_a, df_med_a, df_high_a
-
-def get_elnet_var_distribution(df, iterations, alpha_low, alpha_med, alpha_high):
-
-
-    betas_low_alpha = []
-    betas_med_alpha = []
-    betas_high_alpha = []
-
-    for i in list(range(iterations)): 
-
-        betas_low_a = np.array(df[i].iloc[alpha_low, :])
-        betas_med_a = np.array(df[i].iloc[alpha_med, :])
-        betas_high_a = np.array(df[i].iloc[alpha_high, :])
-    
-        betas_low_alpha.append(betas_low_a)
-        betas_med_alpha.append(betas_med_a)
-        betas_high_alpha.append(betas_high_a)
-    
-        df_low_a = pd.DataFrame(betas_low_alpha)
-        df_med_a = pd.DataFrame(betas_med_alpha)
-        df_high_a = pd.DataFrame(betas_high_alpha)
-        
-    
-    return df_low_a, df_med_a, df_high_a
 
 def generate_true_betas(non_zero_betas, zero_betas, size_nonzero):
 
@@ -331,6 +204,7 @@ def generate_true_betas(non_zero_betas, zero_betas, size_nonzero):
         store_true_betas.append(true_betas)
         
     return store_true_betas
+
 
 def get_predictions(n, p, true_betas, cor_factor, iterations, alphas, X_test): 
 
@@ -393,6 +267,7 @@ def get_predictions(n, p, true_betas, cor_factor, iterations, alphas, X_test):
                            store_predictions_df_elnet_50, store_predictions_df_elnet_70]
         
     return predictions_dfs
+
 
 def compute_mse(predictions_df_list, y_test, iterations):
 
--- auxiliary_files/auxiliary_plots.py
+++ auxiliary_files/auxiliary_plots.py
@@ -35,12 +35,12 @@ def plot_betas(dfs, alphas, rows, columns, reg_type):
     
     plt.savefig(f"{reg_type}_plot_betas.png", bbox_inches='tight')
 
-def plot_betas_lasso(dfs, alphas, rows, columns):
+def plot_betas_lasso(dfs, alphas, rows, columns, sparsity_index):
 
     plt.figure(figsize = (30,30))
     count = 1
 
-    for i, a in zip(dfs, alphas):
+    for i, a, s in zip(dfs, alphas, sparsity_index):
         plt.subplot(rows, columns, count)
     
         ax = plt.gca()
@@ -57,7 +57,7 @@ def plot_betas_lasso(dfs, alphas, rows, columns):
         ax.tick_params(axis='both', which='major', labelsize = 32)
         ax.set_ylim([0, 6])
 
-        plt.title(f"Lasso coefficients as a function of $\lambda$, p = {i.shape[1]} ", fontsize = 40)
+        plt.title(f"Lasso coefficients as a function of $\lambda$, s$_{{{0}}}$ = {s}", fontsize = 40)
         plt.axis("tight")
         
         count += 1
@@ -65,7 +65,7 @@ def plot_betas_lasso(dfs, alphas, rows, columns):
     plt.savefig("lasso_plot_betas.png", bbox_inches='tight')
     
         
-def plot_average_betas(dfs, alphas, reg_type):
+def plot_average_ridge_betas(dfs, alphas, reg_type):
 
     plt.figure(figsize = (40, 25))
     count = 1
@@ -93,35 +93,37 @@ def plot_average_betas(dfs, alphas, reg_type):
         count += 1
     
     plt.savefig(f"{reg_type}_plot_average_betas.png", bbox_inches='tight')
-        
-def plot_betas_single_case(df, alphas, L_w, reg_type):
-    plt.figure(figsize = (20, 10))
+
+
+def plot_average_lasso_betas(dfs, alphas, sparsity_index, reg_type):
+
+    plt.figure(figsize = (40, 25))
     count = 1
 
-    plt.subplot(1, 1, count)
-
-    ax = plt.gca()
-
-    ax.plot(df)
-
-    ax.set_xscale("log")
-    ax.set_xlim(ax.get_xlim()[::-1])  # reverse axis
-
-    plt.xlabel(f"$\lambda$", fontsize = 25)
-    plt.ylabel("weights", fontsize = 25)
-
-    ax.tick_params(axis='both', which='major', labelsize = 20)
-    ax.set_ylim([0, 6])
-    #plt.axhline(y=0, color='black', linestyle='--')
-    if reg_type == "Average elastic net":
-            
-            plt.title(f"{reg_type} coefficients as a function of $\lambda$, p = {df.shape[1]}, L_ratio = {L_w}[count-1] ", fontsize = 28)
+    for i, a, s in zip(dfs, alphas, sparsity_index):
+        plt.subplot(2, 2, count)
+    
+        ax = plt.gca()
+    
+        ax.plot(a, i)
+    
+        ax.set_xscale("log")
+        ax.set_xlim(ax.get_xlim()[::-1])  # reverse axis
+    
+        plt.xlabel(f"$\lambda$", fontsize = 35)
+        plt.ylabel("weights", fontsize = 35)
+    
+        ax.tick_params(axis='both', which='major', labelsize = 32)
+     
+        plt.axhline(y=0, color='black', linestyle='--')
+        plt.axis("tight")
         
-    else:
-           
-            plt.title(f"{reg_type} coefficients as a function of $\lambda$, p = {df.shape[1]} ", fontsize = 28)
-    plt.axis("tight")
-    count += 1
+        plt.title(f"{reg_type} coefficients as a function of $\lambda$, s$_{{{0}}}$ = {s} ", fontsize = 40)
+        
+        count += 1
+    
+    plt.savefig(f"{reg_type}_plot_average_betas.png", bbox_inches='tight')
+       
 
 def plot_average_betas_elnet(dfs, alphas, L_w):
 
@@ -147,7 +149,7 @@ def plot_average_betas_elnet(dfs, alphas, L_w):
         plt.axhline(y=0, color='black', linestyle='--')
         plt.axis("tight")
         
-        plt.title(f"Average elastic net coefficients, p = {i.shape[1]}, L_ratio = {L_w[count-1]} ", fontsize = 40)
+        plt.title(f"Average elastic net coefficients, L_ratio = {L_w[count-1]} ", fontsize = 40)
       
         plt.axis("tight")
         count += 1
@@ -194,64 +196,7 @@ def plot_ols_beta_distribution(df):
     fig_4.set_title(f"Distribution coefficients for p = {df[3].shape[1]}", fontsize=32)
     
     f.savefig('ols_distr.png', bbox_inches='tight')
-
-# delete plot_elnet_beta_distribution_old
     
-def plot_elnet_beta_distribution_old(df_low, df_med, df_high): 
-
-    sns.set(style="white", palette="muted", color_codes=True)
-    #rs = np.random.RandomState(10)
-
-    # Set up the matplotlib figure
-    f, axes = plt.subplots(1, 3, figsize=(18, 8))
-
-    fig_1=sns.kdeplot(data=df_low, ax=axes[0])
-    fig_1.legend([],[], frameon=False)
-    fig_1.title.set_text(f"Distribution of elastic net coefficients for low $\lambda$, p = {df_low.shape[1]}")
-
-    fig_2=sns.kdeplot(data=df_med, ax=axes[1])
-    fig_2.legend([],[], frameon=False)
-    fig_2.title.set_text(f"Distribution of elastic net coefficients for moderate $\lambda$, p = {df_med.shape[1]}")
-
-    fig_3=sns.kdeplot(data=df_high, ax=axes[2])
-    fig_3.legend([],[], frameon=False)
-    fig_3.title.set_text(f"Distribution of elastic net coefficients for high $\lambda$, p = {df_high.shape[1]}")
-    plt.show()
-    
-# delete plot_shrunken_beta_distribution_old
-
-def plot_shrunken_beta_distribution_old(df_low, df_med, df_high, reg_type): 
-
-    sns.set(style="white", palette="muted", color_codes=True)
-
-    # Set up the matplotlib figure
-    f, axes = plt.subplots(1, 3, figsize=(50, 20))
-
-    fig_1=sns.kdeplot(data=df_low, ax=axes[0])
-    fig_1.legend([],[], frameon=False)
-    fig_1.axvline(x=5, color='black', linestyle='--')
-    fig_1.spines["bottom"].set_linestyle("dotted")
-    fig_1.tick_params(labelsize=30)
-    fig_1.set_ylabel("Density", fontsize=35)
-    fig_1.set_title(f"Distribution of {reg_type} coefficients, low $\lambda$, p = {df_low.shape[1]}", fontsize=40)
-
-    fig_2=sns.kdeplot(data=df_med, ax=axes[1])
-    fig_2.legend([],[], frameon=False)
-    fig_2.axvline(x=5, color='black', linestyle='--')
-    fig_2.spines["bottom"].set_linestyle("dotted")
-    fig_2.tick_params(labelsize=30)
-    fig_2.set_ylabel("Density", fontsize=35)
-    fig_2.set_title(f"Distribution of {reg_type} coefficients, moderate $\lambda$, p = {df_med.shape[1]}", fontsize=40)
-
-    fig_3=sns.kdeplot(data=df_high, ax=axes[2])
-    fig_3.legend([],[], frameon=False)
-    fig_3.axvline(x=5, color='black', linestyle='--')
-    fig_3.spines["bottom"].set_linestyle("dotted")
-    fig_3.tick_params(labelsize=30)
-    fig_3.set_ylabel("Density", fontsize=35)
-    fig_3.set_title(f"Distribution of {reg_type} coefficients, high $\lambda$, p = {df_high.shape[1]}", fontsize=40)
-    
-    f.savefig(f"{reg_type}_shrunken_beta_dist_{df_low.shape[1]}.png", bbox_inches='tight')
 
 def plot_shrunken_beta_distribution(df_low, df_med, df_high, reg_type, nonzero_betas_mean, L_weight): 
 
@@ -270,7 +215,7 @@ def plot_shrunken_beta_distribution(df_low, df_med, df_high, reg_type, nonzero_b
         fig_1.spines["bottom"].set_linestyle("dotted")
         fig_1.tick_params(labelsize=40)
         fig_1.set_ylabel("Density", fontsize=45)
-        fig_1.set_title(f"Distribution of {reg_type} coefficients, low $\lambda$, p = {df_low.shape[1]}, L_ratio = {L_weight}", fontsize=50)
+        fig_1.set_title(f"Distribution of {reg_type} coefficients, low $\lambda$, L_ratio = {L_weight}", fontsize=50)
 
         fig_2=sns.kdeplot(data=df_med, ax=axes[1])
         fig_2.legend([],[], frameon=False)
@@ -278,7 +223,7 @@ def plot_shrunken_beta_distribution(df_low, df_med, df_high, reg_type, nonzero_b
         fig_2.spines["bottom"].set_linestyle("dotted")
         fig_2.tick_params(labelsize=40)
         fig_2.set_ylabel("Density", fontsize=45)
-        fig_2.set_title(f"Distribution of {reg_type} coefficients, moderate $\lambda$, p = {df_med.shape[1]}, L_ratio = {L_weight}", fontsize=50)
+        fig_2.set_title(f"Distribution of {reg_type} coefficients, moderate $\lambda$, L_ratio = {L_weight}", fontsize=50)
 
         fig_3=sns.kdeplot(data=df_high, ax=axes[2])
         fig_3.legend([],[], frameon=False)
@@ -286,7 +231,7 @@ def plot_shrunken_beta_distribution(df_low, df_med, df_high, reg_type, nonzero_b
         fig_3.spines["bottom"].set_linestyle("dotted")
         fig_3.tick_params(labelsize=40)
         fig_3.set_ylabel("Density", fontsize=45)
-        fig_3.set_title(f"Distribution of {reg_type} coefficients, high $\lambda$, p = {df_high.shape[1]}, L_ratio = {L_weight}", fontsize=50)
+        fig_3.set_title(f"Distribution of {reg_type} coefficients, high $\lambda$, L_ratio = {L_weight}", fontsize=50)
     
         f.savefig(f"{reg_type}_shrunken_beta_dist_{df_low.shape[1]}_{L_weight}.png", bbox_inches='tight')
     
@@ -300,7 +245,7 @@ def plot_shrunken_beta_distribution(df_low, df_med, df_high, reg_type, nonzero_b
         fig_1.spines["bottom"].set_linestyle("dotted")
         fig_1.tick_params(labelsize=32)
         fig_1.set_ylabel("Density", fontsize=37)
-        fig_1.set_title(f"Distribution of {reg_type} coefficients, low $\lambda$, p = {df_low.shape[1]}", fontsize=40)
+        fig_1.set_title(f"Distribution of {reg_type} coefficients, low $\lambda$", fontsize=40)
 
         fig_2=sns.kdeplot(data=df_med, ax=axes[1])
         fig_2.legend([],[], frameon=False)
@@ -308,7 +253,7 @@ def plot_shrunken_beta_distribution(df_low, df_med, df_high, reg_type, nonzero_b
         fig_2.spines["bottom"].set_linestyle("dotted")
         fig_2.tick_params(labelsize=32)
         fig_2.set_ylabel("Density", fontsize=37)
-        fig_2.set_title(f"Distribution of {reg_type} coefficients, moderate $\lambda$, p = {df_med.shape[1]}", fontsize=40)
+        fig_2.set_title(f"Distribution of {reg_type} coefficients, moderate $\lambda$", fontsize=40)
 
         fig_3=sns.kdeplot(data=df_high, ax=axes[2])
         fig_3.legend([],[], frameon=False)
@@ -316,7 +261,7 @@ def plot_shrunken_beta_distribution(df_low, df_med, df_high, reg_type, nonzero_b
         fig_3.spines["bottom"].set_linestyle("dotted")
         fig_3.tick_params(labelsize=32)
         fig_3.set_ylabel("Density", fontsize=37)
-        fig_3.set_title(f"Distribution of {reg_type} coefficients, high $\lambda$, p = {df_high.shape[1]}", fontsize=40)
+        fig_3.set_title(f"Distribution of {reg_type} coefficients, high $\lambda$", fontsize=40)
     
         f.savefig(f"{reg_type}_shrunken_beta_dist_{df_low.shape[1]}.png", bbox_inches='tight')
 
